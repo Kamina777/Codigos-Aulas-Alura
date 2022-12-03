ce46bd42a7bd3c26c1f9e1b4c7769f69f5750444
Kamina777 <matheus.alvesf@yahoo.com>
2022-12-03 07:12:05 -0300

    curso javascript

---
+++ JavaScript/type-number.js
@@ -0,0 +1,11 @@
+const meuNumero = 3;
+
+const num1 = 2.9;
+const num2 = 3.6;
+
+const operacaoMatematica = num1 / num2;
+console.log(operacaoMatematica);
+
+const alura = "alura";
+console.log(alura);
+
