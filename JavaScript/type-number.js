b2db15712425675ab5df700c8440d43b498077ad
Kamina777 <matheus.alvesf@yahoo.com>
2022-12-05 21:56:19 -0300

    curso javascript

--- JavaScript/type-number.js
+++ JavaScript/type-number.js
@@ -1,11 +1,29 @@
-const meuNumero = 3;
+// const meuNumero = 3;
+// var algumaCoisa = "alguma coisa"; //não é recomendavel usar esse
+// let letVariavel = "variavel let";
 
-const num1 = 2.9;
-const num2 = 3.6;
+// const num1 = 2.9;
+// const num2 = 3.6;
 
-const operacaoMatematica = num1 / num2;
-console.log(operacaoMatematica);
+// const operacaoMatematica = num1 / num2;
+// console.log(operacaoMatematica);
 
-const alura = "alura";
-console.log(alura);
+// const alura = "alura";
+// console.log(alura);
 
+/////////////////
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if(idadeCliente >= idadeMinima)
+// {
+//     console.log("pode entrar");
+// } else
+// {
+//     console.log("sadsfadsf")
+// }
+
+console.log(idadeCliente >= idadeMinima 
+    ?"pode entrar" 
+    : "Sarah lixo");
